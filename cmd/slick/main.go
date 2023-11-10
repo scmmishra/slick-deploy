1372d6d43c4eaa1c73499666470bbb00e72d0e42
Shivam Mishra <scm.mymail@gmail.com>
2023-11-10 09:26:21 +0530

    feat: setup cobra

--- cmd/slick/main.go
+++ cmd/slick/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "slick",
+	Short: "Slick is a CLI tool for zero-downtime deployment using Docker and Caddy",
+	Long: `Slick is designed to simplify and streamline your deployment process
+ensuring zero downtime and easy configuration management.`,
+}
+
+// deployCmd represents the deploy command
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Deploy your application with zero downtime",
+	Long: `The deploy command starts a new deployment process
+ensuring that your application is updated with no service interruption.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Deploying your application...")
+		// deployment logic here
+	},
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(deployCmd)
+}
 
 func main() {
-    fmt.Println("Welcome to Slick Deploy!")
+	Execute()
 }
