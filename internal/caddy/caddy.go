38aef153b0bf13238b4889b11eb99bc6b67c275d
Shivam Mishra <scm.mymail@gmail.com>
2023-11-13 21:31:55 +0530

    feat: lets just use caddyfiles

--- internal/caddy/caddy.go
+++ internal/caddy/caddy.go
@@ -1,93 +1,64 @@
 package caddy
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strings"
 
 	"github.com/scmmishra/slick-deploy/internal/config"
 )
 
-func (c *CaddyClient) GetCurrentConfig() (*CaddyConfig, error) {
-	resp, err := c.HTTPClient.Get(c.BaseURL + "/config/")
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	bodyStr := string(body)
-
-	if bodyStr == "null\n" {
-		return nil, nil
-	}
-
-	var config CaddyConfig
-	err = json.Unmarshal(body, &config)
-	if err != nil {
-		return nil, err
-	}
-
-	return &config, nil
+type CaddyClient struct {
+	BaseURL    string
+	HTTPClient *http.Client
 }
 
-func (c *CaddyClient) BootStrapCaddy() error {
-	basicConfig := DefaultConfig()
-	configJSON, err := json.Marshal(basicConfig)
+func NewCaddyClient(baseURL string) *CaddyClient {
+	return &CaddyClient{
+		BaseURL:    baseURL,
+		HTTPClient: &http.Client{},
+	}
+}
 
-	if err != nil {
-		return fmt.Errorf("error marshaling config: %w", err)
+type Rule struct {
+	Match        string         `yaml:"match"`
+	ReverseProxy []ReverseProxy `yaml:"reverse_proxy"`
+}
+
+type ReverseProxy struct {
+	Path string `yaml:"path"`
+	To   string `yaml:"to"`
+}
+
+type DataWithPort struct {
+	Port int
+}
+
+// ConvertToCaddyfile translates the CaddyConfig struct to a Caddyfile string
+func ConvertToCaddyfile(caddyCfg config.CaddyConfig, port int) (string, error) {
+	var caddyfileBuilder strings.Builder
+
+	for _, rule := range caddyCfg.Rules {
+		caddyfileBuilder.WriteString(rule.Match)
+		caddyfileBuilder.WriteString(" {\n")
+		for _, proxy := range rule.ReverseProxy {
+			toPath := strings.ReplaceAll(proxy.To, "{port}", fmt.Sprintf("%d", port))
+
+			caddyfileBuilder.WriteString(fmt.Sprintf("  reverse_proxy %s %s\n", proxy.Path, toPath))
+		}
+		caddyfileBuilder.WriteString("}\n\n")
 	}
 
-	// Send request to Caddy to update config
-	req, err := http.NewRequest("POST", c.BaseURL+"/load", bytes.NewBuffer(configJSON))
-	if err != nil {
-		return fmt.Errorf("error creating request: %w", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("error sending request to Caddy: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("received non-OK response from Caddy: %s", resp.Status)
-	}
-
-	return nil
+	return caddyfileBuilder.String(), nil
 }
 
 func SetupCaddy(port int, cfg config.DeploymentConfig) error {
-	client := NewCaddyClient(cfg.Caddy.AdminAPI)
-
-	caddyConfig, err := client.GetCurrentConfig()
+	caddyfile, err := ConvertToCaddyfile(cfg.Caddy, port)
 	if err != nil {
 		return err
 	}
-
-	if caddyConfig == nil {
-		fmt.Println("Empty Caddy config, bootstrapping...")
-		err = client.BootStrapCaddy()
-		if err != nil {
-			return err
-		}
-
-		// fetch the config again
-		caddyConfig, _ = client.GetCurrentConfig()
-	}
-
-	// add the reverse proxy
-
-	fmt.Println("Current Caddy config")
-	fmt.Println(caddyConfig.Apps.HTTP)
+	fmt.Println("New Caddy config")
+	fmt.Println(caddyfile)
 
 	return nil
 }
