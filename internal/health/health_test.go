dd665da780a98691d87d303cdde08e7a977a75ef
Shivam Mishra <scm.mymail@gmail.com>
2023-11-23 16:30:32 +0530

    test: health

--- internal/health/health_test.go
+++ internal/health/health_test.go
@@ -1,51 +1,92 @@
 package health
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/scmmishra/slick-deploy/internal/config"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCheckHealth(t *testing.T) {
-	tests := []struct {
-		name     string
-		host     string
-		endpoint string
-		wantErr  bool
-	}{
-		{
-			name:     "Healthy host",
-			host:     "http://shivam.dev",
-			endpoint: "/health",
-			wantErr:  false,
-		},
-		{
-			name:     "Unhealthy host",
-			host:     "http://unhealthy.shivam.dev",
-			endpoint: "/health",
-			wantErr:  true,
-		},
-		{
-			name:     "Empty host",
-			host:     "",
-			endpoint: "",
-			wantErr:  false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := CheckHealth(tt.host, &config.HealthCheck{
-				Endpoint:       tt.endpoint,
-				TimeoutSeconds: 5,
-			})
-
-			if tt.wantErr {
-				assert.Error(t, err, "CheckHealth() should return an error")
-			} else {
-				assert.NoError(t, err, "CheckHealth() should not return an error")
-			}
-		})
-	}
+// setupTestServer helps in creating a test HTTP server.
+func setupTestServer(handler http.HandlerFunc) (string, func()) {
+	ts := httptest.NewServer(handler)
+	return ts.URL, func() { ts.Close() }
 }
+
+func TestCheckHealth_EmptyHost(t *testing.T) {
+	t.Parallel()
+
+	err := CheckHealth("", &config.HealthCheck{
+		Endpoint:       "/health",
+		TimeoutSeconds: 5,
+	})
+
+	assert.NoError(t, err, "Expected no error for healthy response")
+}
+
+func TestCheckHealth_EmptyEndpoint(t *testing.T) {
+	t.Parallel()
+
+	err := CheckHealth("https://shivam.dev", &config.HealthCheck{
+		Endpoint:       "",
+		TimeoutSeconds: 5,
+	})
+
+	assert.NoError(t, err, "Expected no error for healthy response")
+}
+
+// TestCheckHealth_Success tests successful health check.
+func TestCheckHealth_Success(t *testing.T) {
+	t.Parallel()
+
+	serverURL, teardown := setupTestServer(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK) // Return 200 OK
+	})
+	defer teardown()
+
+	err := CheckHealth(serverURL, &config.HealthCheck{
+		Endpoint:       "/health",
+		TimeoutSeconds: 5,
+	})
+
+	assert.NoError(t, err, "Expected no error for healthy response")
+}
+
+// TestCheckHealth_ServerError tests the health check for server error response.
+func TestCheckHealth_ServerError(t *testing.T) {
+	t.Parallel()
+
+	serverURL, teardown := setupTestServer(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError) // Return 500 Internal Server Error
+	})
+	defer teardown()
+
+	err := CheckHealth(serverURL, &config.HealthCheck{
+		Endpoint:       "/health",
+		TimeoutSeconds: 5,
+	})
+
+	assert.Error(t, err, "Expected an error for server error response")
+}
+
+// TestCheckHealth_NetworkError tests the health check for a network error.
+func TestCheckHealth_NetworkError(t *testing.T) {
+	t.Parallel()
+
+	serverURL, teardown := setupTestServer(func(w http.ResponseWriter, r *http.Request) {
+		// You can configure the response here if needed
+	})
+	// Close the server immediately to simulate a network error
+	teardown()
+
+	err := CheckHealth(serverURL, &config.HealthCheck{
+		Endpoint:       "/health",
+		TimeoutSeconds: 5,
+	})
+
+	assert.Error(t, err, "Expected a network error")
+}
+
+// Additional test cases can be added here
