c06f26962fdbff3f7db62896e3ba1e0df77339c1
Shivam Mishra <scm.mymail@gmail.com>
2023-11-30 19:20:49 +0530

    test: error conditions for client

--- internal/caddy/client_test.go
+++ internal/caddy/client_test.go
@@ -22,3 +22,43 @@ func TestLoad(t *testing.T) {
 		t.Errorf("Expected no error, got %v", err)
 	}
 }
+
+func TestLoad_ErrorConditions(t *testing.T) {
+	// Test when the request creation fails
+	{
+		client := &CaddyClient{
+			BaseURL:    ":", // invalid URL
+			HTTPClient: &http.Client{},
+		}
+		err := client.Load("test caddyfile")
+		if err == nil {
+			t.Errorf("Expected error, got nil")
+		}
+	}
+
+	// Test when the HTTP client fails to send the request
+	{
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.WriteHeader(http.StatusOK)
+		}))
+		server.Close() // close the server to simulate network error
+		client := NewCaddyClient(server.URL)
+		err := client.Load("test caddyfile")
+		if err == nil {
+			t.Errorf("Expected error, got nil")
+		}
+	}
+
+	// Test when the server returns a non-200 status code
+	{
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.WriteHeader(http.StatusInternalServerError) // return 500 status code
+		}))
+		defer server.Close()
+		client := NewCaddyClient(server.URL)
+		err := client.Load("test caddyfile")
+		if err == nil {
+			t.Errorf("Expected error, got nil")
+		}
+	}
+}
