c0c215f12612a15b138826bbe418af6c155963d8
Shivam Mishra <scm.mymail@gmail.com>
2024-08-08 21:12:09 +0530

    fix: Unused parameter in function

--- cmd/slick/main.go
+++ cmd/slick/main.go
@@ -24,7 +24,7 @@ var deployCmd = &cobra.Command{
 	Use:   "deploy",
 	Short: "Deploy your application with zero downtime",
 	Long:  "The deploy command starts a new deployment process ensuring that your application is updated with no service interruption.",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(cmd *cobra.Command, _ []string) error {
 		return runDeploy(cmd, defaultDeployer, defaultConfigLoader)
 	},
 }
@@ -33,7 +33,7 @@ var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Get the status of your application",
 	Long:  "The status command shows the status of your application.",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(_ *cobra.Command, __ []string) error {
 		return runStatus()
 	},
 }
@@ -42,7 +42,7 @@ var logsCmd = &cobra.Command{
 	Use:   "logs",
 	Short: "Tail and follow app logs",
 	Long:  "The logs command shows the logs output of your application. It is similar to running 'docker logs -f <container-id>'",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(cmd *cobra.Command, _ []string) error {
 		return runLogs(cmd, defaultConfigLoader)
 	},
 }
@@ -51,7 +51,7 @@ var caddyInspectCmd = &cobra.Command{
 	Use:   "caddy-inspect",
 	Short: "Inspect the current Caddy configuration",
 	Long:  "The caddy-inspect command retrieves and displays the current Caddy configuration.",
-	RunE: func(cmd *cobra.Command, args []string) error {
+	RunE: func(cmd *cobra.Command, _ []string) error {
 		return runCaddyInspect(cmd, defaultConfigLoader)
 	},
 }
